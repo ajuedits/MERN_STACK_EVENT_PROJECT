690231266baa608a929b876ffbf8cae0036a40db
ajuedits <ajuedits01@gmail.com>
2024-04-16 15:18:56 +0530

    second commit

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -13,7 +13,16 @@ app.use (cors({
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
+app.get("/",(req,res) =>{
 
+    res.json({
+        success:true,
+        message :"hello hello"
+    });
+}
+
+);
+  
 app.use("/api/v1/message",messageRouter);
 
 dbConnection();
